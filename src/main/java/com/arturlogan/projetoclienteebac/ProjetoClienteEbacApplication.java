d14db27f499bb1dd4a041bde4c935e3a0c5c8bf8
Artur Logan <arturlogan1ferreira@gmail.com>
2024-04-29 19:58:53 -0300

    Primeiro Commit

---
+++ src/main/java/com/arturlogan/projetoclienteebac/ProjetoClienteEbacApplication.java
@@ -0,0 +1,13 @@
+package com.arturlogan.projetoclienteebac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoClienteEbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoClienteEbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturlogan/projetoclienteebac/ProjetoClienteEbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturlogan.projetoclienteebac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoClienteEbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
